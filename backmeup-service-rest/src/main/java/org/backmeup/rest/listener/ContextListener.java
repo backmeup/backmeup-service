a0c86ca0da90b3beb8f422ab0b1d23ba2bf8bc0a
Peter Kofler <peter.kofler@code-cop.org>
2014-03-16 20:20:30 +0100

    remove duplication from session listener and base restlet into new class LogicContextHolder

--- backmeup-service-rest/src/main/java/org/backmeup/rest/listener/ContextListener.java
+++ backmeup-service-rest/src/main/java/org/backmeup/rest/listener/ContextListener.java
@@ -1,31 +1,44 @@
 package org.backmeup.rest.listener;
 
+import javax.servlet.ServletContext;
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
 
 import org.backmeup.logic.BusinessLogic;
+import org.backmeup.rest.BusinessLogicContextHolder;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+/**
+ * Shutdown the business logic when application stops.
+ */
 public class ContextListener implements ServletContextListener {
-	private final Logger logger = LoggerFactory.getLogger(ContextListener.class);
-	
-	@Override
-	public void contextInitialized(ServletContextEvent sce) {
 
-	}
+    private final Logger logger = LoggerFactory.getLogger(getClass());
 
-	@Override
-	public void contextDestroyed(ServletContextEvent sce) {
-		logger.info("Shutting down business logic...");
-		
-		// call shutdown on exit
-		BusinessLogic logic = (BusinessLogic) sce.getServletContext().getAttribute("org.backmeup.logic");
-		if(logic != null) {
-			logic.shutdown();
-			logic = null;
-		}
+    @Override
+    public void contextInitialized(ServletContextEvent sce) {
+    }
 
-		logger.info("Shutdown completed");
-	}
+    @Override
+    public void contextDestroyed(ServletContextEvent sce) {
+        // call shutdown on exit
+        shutdownBusinessLogicFacade(sce.getServletContext());
+    }
+
+    private void shutdownBusinessLogicFacade(ServletContext servletContext) {
+        logger.info("Shutting down business logic...");
+
+        BusinessLogicContextHolder contextHolder = new BusinessLogicContextHolder(servletContext);
+
+        BusinessLogic logic = contextHolder.get();
+        if (logic != null) {
+            logic.shutdown();
+        }
+        
+        contextHolder.remove();
+
+        logger.info("Shutdown completed");
+    }
+
 }
