c2b046718024136cdf055d8fb9b248ce995b39ef
Peter Kofler <peter.kofler@code-cop.org>
2014-03-18 16:42:41 +0100

    change BusinessLogicContextResolver to use the JNDI provided bean manager instead of asking Weld directly

--- backmeup-service-rest/src/main/java/org/backmeup/rest/provider/BusinessLogicContextResolver.java
+++ backmeup-service-rest/src/main/java/org/backmeup/rest/provider/BusinessLogicContextResolver.java
@@ -4,37 +4,42 @@ import javax.ws.rs.ext.ContextResolver;
 import javax.ws.rs.ext.Provider;
 
 import org.backmeup.logic.BusinessLogic;
-//import org.jboss.weld.environment.se.Weld;
-//import org.jboss.weld.environment.se.WeldContainer;
+import org.backmeup.rest.cdi.JNDIBeanManager;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 /**
- * This class looks up the BusinessLogic implementation
- * by using Weld.
+ * This class looks up the BusinessLogic implementation by using Weld.
  * 
  * @author fschoeppl
- *
  */
 @Provider
 public class BusinessLogicContextResolver implements ContextResolver<BusinessLogic> {
-	private final Logger logger = LoggerFactory.getLogger(BusinessLogicContextResolver.class); 
-	private BusinessLogic logic;
-	
-	public BusinessLogic getContext(Class<?> arg0) {	
-		if (logic == null) {
-			try {
-//				Weld weld = new Weld();
-//			    WeldContainer container = weld.initialize();			    
-//			    logic = container.instance().select(BusinessLogic.class).get();
-			} catch (Throwable e) {
-				do {			
-					logger.error("", e);
-					e = e.getCause();
-				} while (e.getCause() != e && e.getCause() != null);
-			}
-		}
-		return logic;
-	}
+
+    private final Logger logger = LoggerFactory.getLogger(getClass());
+
+    private BusinessLogic logic;
+
+    @Override
+    public BusinessLogic getContext(Class<?> type) {
+        if (logic == null) {
+            try {
+                logic = fetchLogicFromJndi();
+            } catch (Throwable e) {
+                logger.error("", e);
+            }
+        }
+        return logic;
+    }
+
+    private BusinessLogic fetchLogicFromJndi() {
+        try {
+            JNDIBeanManager jndiManager = JNDIBeanManager.getInstance();
+            return jndiManager.getBean(BusinessLogic.class);
+        } catch (Exception e) {
+            logger.error("", e);
+            return null;
+        }
+    }
 
 }
