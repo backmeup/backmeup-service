ca7af5f0979d526e5bfc3fa029026b1ae1a9c7b0
Sebastian Pimminger <sebastian.pimminger@gmail.com>
2014-05-02 15:34:45 +0200

    Abstract class for Storage should actually be an interface (for osgi bundles)

--- backmeup-plugin-api/src/main/java/org/backmeup/plugin/api/storage/Storage.java
+++ backmeup-plugin-api/src/main/java/org/backmeup/plugin/api/storage/Storage.java
@@ -5,33 +5,31 @@ import java.util.Iterator;
 
 import org.backmeup.plugin.api.MetainfoContainer;
 
-public abstract class Storage {
-	
-	public abstract void open(String path) throws StorageException;
-	
-	public abstract void close() throws StorageException;	
-	
-	public abstract void delete() throws StorageException;
-	
+public interface Storage {
+	void open(String path) throws StorageException;
+
+	void close() throws StorageException;
+
+	void delete() throws StorageException;
+
 	/** Read methods **/
-	
-	public abstract int getDataObjectCount() throws StorageException;
-	
+
+	int getDataObjectCount() throws StorageException;
+
 	// The total size of all objects within this storage in bytes
-	public abstract long getDataObjectSize() throws StorageException;
-	
-	public abstract Iterator<DataObject> getDataObjects() throws StorageException;
-	
-	public abstract boolean existsPath(String path) throws StorageException;
-	
+	long getDataObjectSize() throws StorageException;
+
+	Iterator<DataObject> getDataObjects() throws StorageException;
+
+	boolean existsPath(String path) throws StorageException;
+
 	/** Write methods **/
-	
-	public abstract void addFile(InputStream is, String path, MetainfoContainer metadata) throws StorageException;
-	
-	public abstract void removeFile(String path) throws StorageException;
-	
-	public abstract void removeDir(String path) throws StorageException;
-	
-	public abstract void move(String fromPath, String toPath) throws StorageException;
-	
+
+	void addFile(InputStream is, String path, MetainfoContainer metadata) throws StorageException;
+
+	void removeFile(String path) throws StorageException;
+
+	void removeDir(String path) throws StorageException;
+
+	void move(String fromPath, String toPath) throws StorageException;
 }
