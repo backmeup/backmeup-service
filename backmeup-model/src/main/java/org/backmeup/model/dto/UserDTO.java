50e0a1ec8fd2138f03835c23d67345240afb98dd
Sebastian Pimminger <sebastian.pimminger@gmail.com>
2014-06-17 09:08:31 +0200

    Added dto class for user

---
+++ backmeup-model/src/main/java/org/backmeup/model/dto/UserDTO.java
@@ -0,0 +1,83 @@
+package org.backmeup.model.dto;
+
+import javax.xml.bind.annotation.XmlRootElement;
+
+@XmlRootElement
+@SuppressWarnings("unused")
+public class UserDTO {
+    private Long userId;
+    private String firstname;
+    private String name;
+    private String password;
+    private String email;
+    private boolean activated;
+    private String verificationKey;
+    
+    public UserDTO() {
+    	
+    }
+
+    public UserDTO(String firstname, String name, String password, String email) {
+    	this.firstname = firstname;
+    	this.name = name;
+    	this.password = password;
+    	this.email = email;
+    	this.activated = false;
+    }
+
+	public String getFirstname() {
+		return firstname;
+	}
+
+	public void setFirstname(String firstname) {
+		this.firstname = firstname;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public String getPassword() {
+		return password;
+	}
+	
+	private void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public Long getUserId() {
+		return userId;
+	}
+
+	private void setUserId(Long userId) {
+		this.userId = userId;
+	}
+
+	public boolean isActivated() {
+		return activated;
+	}
+
+	private void setActivated(Boolean activated) {
+		this.activated = activated;
+	}
+
+	public String getVerificationKey() {
+		return verificationKey;
+	}
+
+	private void setVerificationKey(String verificationKey) {
+		this.verificationKey = verificationKey;
+	}
+}
