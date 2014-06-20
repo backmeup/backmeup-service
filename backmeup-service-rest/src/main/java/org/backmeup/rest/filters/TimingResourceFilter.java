22dc9b86517dc7abb52c6d77c7f7b9d3cf217e09
Sebastian Pimminger <sebastian.pimminger@gmail.com>
2014-06-20 08:50:43 +0200

    Filter to measure response time for each request

---
+++ backmeup-service-rest/src/main/java/org/backmeup/rest/filters/TimingResourceFilter.java
@@ -0,0 +1,52 @@
+package org.backmeup.rest.filters;
+
+import java.io.IOException;
+
+import javax.ws.rs.container.ContainerRequestContext;
+import javax.ws.rs.container.ContainerRequestFilter;
+import javax.ws.rs.container.ContainerResponseContext;
+import javax.ws.rs.container.ContainerResponseFilter;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Timing filter on resources. Monitors how long it takes to process request.
+ */
+public class TimingResourceFilter implements ContainerRequestFilter, ContainerResponseFilter {
+	private final static Logger logger = LoggerFactory.getLogger(TimingResourceFilter.class);
+	private final static TimerThreadLocal timer = new TimerThreadLocal();
+
+	public TimingResourceFilter() {
+
+	}
+
+	@Override
+	public void filter(ContainerRequestContext request, ContainerResponseContext response) throws IOException {
+		long reqProcessingTimeInMs = timer.stop();
+		logger.info("Request processing time: " + reqProcessingTimeInMs + "ms");
+	}
+
+	@Override
+	public void filter(ContainerRequestContext request) throws IOException {
+		timer.start();
+
+	}
+
+	private final static class TimerThreadLocal extends ThreadLocal<Long> {
+		public long start() {
+			long value = System.currentTimeMillis();
+			this.set(value);
+			return value;
+		}
+
+		public long stop() {
+			return System.currentTimeMillis() - get();
+		}
+
+		@Override
+		protected Long initialValue() {
+			return System.currentTimeMillis();
+		}
+	}
+}
