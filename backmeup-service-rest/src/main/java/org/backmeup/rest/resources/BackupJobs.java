e601a687a449eba62204ac3c3849d1e7d408b15a
Sebastian Pimminger <sebastian.pimminger@gmail.com>
2014-06-21 16:31:34 +0200

    Initial implementation of backupjobs resource

---
+++ backmeup-service-rest/src/main/java/org/backmeup/rest/resources/BackupJobs.java
@@ -0,0 +1,22 @@
+package org.backmeup.rest.resources;
+
+import javax.ws.rs.DefaultValue;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.MediaType;
+
+import org.backmeup.model.dto.BackupJobDTO;
+import org.backmeup.rest.DummyDataManager;
+
+@Path("/backupjobs")
+public class BackupJobs extends Base {	
+	@GET
+	@Path("/{jobId}")
+	@Produces(MediaType.APPLICATION_JSON)
+	public BackupJobDTO getPlugin(@PathParam("jobId") String pluginId, @QueryParam("expandProtocol") @DefaultValue("false") boolean expandProtocol) {
+		return DummyDataManager.getBackupJobDTO(true);
+	}
+}
