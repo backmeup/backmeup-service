496e93ee48534c05e7d0ab0b45451c40edb1d4e7
Sebastian Pimminger <sebastian.pimminger@gmail.com>
2014-07-11 13:44:16 +0200

    Added validity field to set the date of the next backup in token dto

--- backmeup-model/src/main/java/org/backmeup/model/dto/TokenDTO.java
+++ backmeup-model/src/main/java/org/backmeup/model/dto/TokenDTO.java
@@ -6,9 +6,20 @@ import javax.xml.bind.annotation.XmlRootElement;
 public class TokenDTO {
 	private Long id;
 	private String token;
+	private Long validity;
 
 	public TokenDTO() {
 
+	}
+
+	public TokenDTO(Long id, String token) {
+		this(id, token, null);
+	}
+
+	public TokenDTO(Long id, String token, Long validity) {
+		this.id = id;
+		this.token = token;
+		this.validity = validity;
 	}
 
 	public String getToken() {
@@ -25,5 +36,13 @@ public class TokenDTO {
 
 	public void setTokenId(Long tokenId) {
 		this.id = tokenId;
+	}
+
+	public Long getValidity() {
+		return validity;
+	}
+
+	public void setValidity(Long validity) {
+		this.validity = validity;
 	}
 }
