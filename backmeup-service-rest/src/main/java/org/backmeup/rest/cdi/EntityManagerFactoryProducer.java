5384a12704dbde365343f54cad75f01bbea1470c
Peter Kofler <peter.kofler@code-cop.org>
2014-10-27 21:56:15 +0100

    format

--- backmeup-service-rest/src/main/java/org/backmeup/rest/cdi/EntityManagerFactoryProducer.java
+++ backmeup-service-rest/src/main/java/org/backmeup/rest/cdi/EntityManagerFactoryProducer.java
@@ -6,23 +6,21 @@ import javax.enterprise.inject.Produces;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 /**
- * This class creates one single EntityManager for the whole application. 
- * It will be injected into the connection class of the business layer.
+ * This class creates one single EntityManager for the whole application. It
+ * will be injected into the connection class of the business layer.
  */
 public class EntityManagerFactoryProducer {
-	private final Logger logger = LoggerFactory.getLogger(EntityManagerFactoryProducer.class); 
-	@Produces
-	@ApplicationScoped
-	public EntityManagerFactory create() {
-		return Persistence.createEntityManagerFactory("org.backmeup.jpa");
-	}
-	
-	public void destroy(@Disposes EntityManagerFactory factory) {
-	  logger.debug("Closing EntityManagerFactory!");
-		factory.close();
-	}
+
+    private static final String PERSISTENCE_UNIT = "org.backmeup.jpa";
+
+    @Produces
+    @ApplicationScoped
+    public EntityManagerFactory create() {
+        return Persistence.createEntityManagerFactory(PERSISTENCE_UNIT);
+    }
+
+    public void destroy(@Disposes EntityManagerFactory factory) {
+        factory.close();
+    }
 }
