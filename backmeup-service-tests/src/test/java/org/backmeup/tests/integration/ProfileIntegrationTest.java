c87fe94fabe7159dc7c101855dcde9f1b776c8b6
Sebastian Pimminger <sebastian.pimminger@gmail.com>
2014-11-03 09:58:19 +0100

    Added integration test for adding profiles Removed obsolete tests

--- backmeup-service-tests/src/test/java/org/backmeup/tests/integration/ProfileIntegrationTest.java
+++ backmeup-service-tests/src/test/java/org/backmeup/tests/integration/ProfileIntegrationTest.java
@@ -264,6 +264,120 @@ public class ProfileIntegrationTest extends IntegrationTestBase {
 	}
 	
 	@Test
+	public void testAddProfileSourceFilegenerator() {	
+		String username = "john.doe";
+		String firstname = "John";
+		String lastname = "Doe";
+		String password = "password1";
+		String email = "john.doe@example.com";
+		
+		String userId = "";
+		String accessToken = "";
+		
+		String pluginId = "org.backmeup.filegenerator";
+		String profileName = "FilegeneratorProfile";
+		PluginType profileType = PluginType.Source;
+		String profileId = "";
+		
+		PluginProfileDTO pluginProfile = new PluginProfileDTO();
+		pluginProfile.setTitle(profileName);
+		pluginProfile.setPluginId(pluginId);
+		pluginProfile.setProfileType(profileType);
+		
+		pluginProfile.addProperty("text", "true");
+		pluginProfile.addProperty("image", "true");
+		pluginProfile.addProperty("pdf", "true");
+		pluginProfile.addProperty("binary", "true");
+		
+		try {
+			ValidatableResponse response = BackMeUpUtils.addUser(username, firstname, lastname, password, email);
+			userId = response.extract().path("userId").toString();
+			accessToken = userId + ";" + password;
+						
+			response = 
+			given()
+				.log().all()
+				.contentType("application/json")
+				.header("Accept", "application/json")
+				.header("Authorization", accessToken)
+				.body(pluginProfile, ObjectMapperType.JACKSON_1)
+			.when()
+				.post("/plugins/" + pluginId)
+			.then()
+				.log().all()
+				.statusCode(200);
+			
+			profileId = response.extract().path("profileId").toString();
+		} finally {
+			BackMeUpUtils.deleteProfile(accessToken, pluginId, profileId);
+			BackMeUpUtils.deleteUser(accessToken, userId);
+		}
+	}
+	
+	@Ignore
+	@Test
+	public void testAddProfileSourceEmail() {	
+		String username = "john.doe";
+		String firstname = "John";
+		String lastname = "Doe";
+		String password = "password1";
+		String email = "john.doe@example.com";
+		
+		String userId = "";
+		String accessToken = "";
+		
+		String pluginId = "org.backmeup.mail";
+		String profileName = "MailProfile";
+		PluginType profileType = PluginType.Source;
+		String profileId = "";
+		
+		String authName = "EmailWork";
+		String authDataId = "";
+		Map<String, String> authProps = new HashMap<>();
+		authProps.put("Username", "");
+		authProps.put("Password", "");
+		authProps.put("Type", "IMAP");
+		authProps.put("Host", "");
+		authProps.put("Port", "");
+		authProps.put("SSL", "");
+		AuthDataDTO authData = new AuthDataDTO();
+		
+		PluginProfileDTO pluginProfile = new PluginProfileDTO();
+		pluginProfile.setTitle(profileName);
+		pluginProfile.setPluginId(pluginId);
+		pluginProfile.setProfileType(profileType);
+		pluginProfile.setAuthData(authData);
+			
+		try {
+			ValidatableResponse response = BackMeUpUtils.addUser(username, firstname, lastname, password, email);
+			userId = response.extract().path("userId").toString();
+			accessToken = userId + ";" + password;
+			
+			response = BackMeUpUtils.addAuthData(accessToken, pluginId, authName, authProps);
+			authDataId = response.extract().path("id").toString();
+			authData.setId(Long.parseLong(authDataId));
+			
+			response = 
+			given()
+				.log().all()
+				.contentType("application/json")
+				.header("Accept", "application/json")
+				.header("Authorization", accessToken)
+				.body(pluginProfile, ObjectMapperType.JACKSON_1)
+			.when()
+				.post("/plugins/" + pluginId)
+			.then()
+				.log().all()
+				.statusCode(200);
+			
+			profileId = response.extract().path("profileId").toString();
+		} finally {
+			BackMeUpUtils.deleteProfile(accessToken, pluginId, profileId);
+			BackMeUpUtils.deleteUser(accessToken, userId);
+		}
+	}
+	
+	@Test
 	public void testGetProfileSourceDummy() {
 		String username = "john.doe";
 		String firstname = "John";
@@ -461,62 +575,10 @@ public class ProfileIntegrationTest extends IntegrationTestBase {
 		}
 	}
 	
-	@Ignore
-	@Test
-	public void testAddProfileFileGeneratorSourceInputBased() {
-		String username = "john.doe";
-		String firstname = "John";
-		String lastname = "Doe";
-		String password = "password1";
-		String email = "john.doe@example.com";
-		
-		String userId = "";
-		String accessToken = "";
-		
-		String pluginId = "org.backmeup.filegenerator";
-		String profileName = "FilegeneratorProfile";
-		PluginType profileType = PluginType.Source;
-		String profileId = "";
-		
-		try {
-			ValidatableResponse response = BackMeUpUtils.addUser(username, firstname, lastname, password, email);
-			userId = response.extract().path("userId").toString();
-			accessToken = userId + ";" + password;
-			
-			PluginProfileDTO pluginProfile = new PluginProfileDTO();
-			pluginProfile.setTitle(profileName);
-			pluginProfile.setPluginId(pluginId);
-			pluginProfile.setProfileType(profileType);
-			
-			pluginProfile.addProperty("text", "true");
-			pluginProfile.addProperty("image", "true");
-			pluginProfile.addProperty("pdf", "true");
-			pluginProfile.addProperty("binary", "true");
-			
-			response = 
-			given()
-				.log().all()
-				.contentType("application/json")
-				.header("Accept", "application/json")
-				.header("Authorization", accessToken)
-				.body(pluginProfile, ObjectMapperType.JACKSON_1)
-			.when()
-				.post("/plugins/" + pluginId)
-			.then()
-				.log().all()
-				.statusCode(200);
-			
-			profileId = response.extract().path("profileId").toString();
-		
-		} finally {
-			BackMeUpUtils.deleteProfile(accessToken, pluginId, profileId);
-			BackMeUpUtils.deleteUser(accessToken, userId);
-		}
-	}
 	
 	@Ignore
 	@Test
-	public void testAddPluginProfileSourceOAuth() {
+	public void testAddPluginProfileDropbox() {
 		String username = "john.doe";
 		String firstname = "John";
 		String lastname = "Doe";
@@ -563,64 +625,4 @@ public class ProfileIntegrationTest extends IntegrationTestBase {
 			BackMeUpUtils.deleteUser(accessToken, username);
 		}
 	}
-/*
-	@Test
-	public void testUpdateNonExistingProfile() {
-		String profileId = "100";
-		String password = "password1";
-		String keyTest = "keyTest";
-		String valueTest = "valueTest";
-		
-		given()
-			.log().all()
-			.contentType("application/x-www-form-urlencoded")
-			.header("Accept", "application/json")
-			.formParam("keyRing", password)
-			.formParam(keyTest, valueTest)
-		.when()
-			.post("/profiles/" + profileId)
-		.then()
-			.assertThat().statusCode(400)
-			.body("errorType", equalTo("java.lang.IllegalArgumentException"));
-	}
-	
-	@Test
-	public void testUpdateProfile() {
-		String username = "TestUser1";
-		String password = "password1";
-		String keyRingPassword = "keyringpassword1";
-		String email = "TestUser@trash-mail.com";
-		
-		String datasourceId = "org.backmeup.dropbox";
-		String profileName = "Dropbox";
-		
-		String profileId = "";
-		
-		String keyTest = "keyTest";
-		String valueTest = "valueTest";
-		
-		try {
-			ValidatableResponse response = BackMeUpUtils.addUser(username, password, keyRingPassword, email);
-			String verificationKey = response.extract().path("verificationKey");
-			BackMeUpUtils.verifyEmail(verificationKey);
-			
-			response = BackMeUpUtils.authenticateDatasource(username, password, profileName, datasourceId);
-			profileId = response.extract().path("profileId");
-						
-			given()
-				.contentType("application/x-www-form-urlencoded")
-				.header("Accept", "application/json")
-				.formParam("keyRing", password)
-				.formParam(keyTest, valueTest)
-			.when()
-				.post("/profiles/" + profileId)
-			.then()
-				.log().all()
-				.assertThat().statusCode(204);
-		} finally {
-			BackMeUpUtils.deleteDatasourceProfile(username, profileId);
-			BackMeUpUtils.deleteUser(username);
-		}
-	}	
-*/
 }
