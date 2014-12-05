3670c404963896c7f82ed0926dafba98a7f293f6
Peter Kofler <peter.kofler@code-cop.org>
2014-12-05 01:52:01 +0100

    add EntityManagerProducer for later

---
+++ backmeup-service-rest/src/main/java/org/backmeup/rest/cdi/EntityManagerProducer.java
@@ -0,0 +1,26 @@
+package org.backmeup.rest.cdi;
+
+import javax.enterprise.context.RequestScoped;
+import javax.enterprise.inject.Disposes;
+import javax.enterprise.inject.Produces;
+import javax.inject.Inject;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+
+public class EntityManagerProducer {
+
+    @Inject
+    private EntityManagerFactory entityManagerFactory;
+
+    @Produces
+    @RequestScoped
+    public EntityManager create() {
+        return this.entityManagerFactory.createEntityManager();
+    }
+
+    public void destroy(@Disposes EntityManager manager) {
+        if (manager.isOpen())
+            manager.close();
+    }
+
+}
