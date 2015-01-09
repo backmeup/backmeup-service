8808d041e1b31f7b7277f1e811a111352327d65e
Andrew Lindley <andrew.lindley@ait.ac.at>
2015-01-09 09:57:35 +0100

    added userID logging for jobID

--- backmeup-core/src/main/java/org/backmeup/job/impl/rabbitmq/RabbitMQJobManager.java
+++ backmeup-core/src/main/java/org/backmeup/job/impl/rabbitmq/RabbitMQJobManager.java
@@ -20,85 +20,86 @@ import com.rabbitmq.client.Connection;
 import com.rabbitmq.client.ConnectionFactory;
 
 /**
- * An implementation of {@link AkkaJobManager} that pushes backup jobs into 
- * a RabbitMQ queue, where they can be handled by worker nodes.
+ * An implementation of {@link AkkaJobManager} that pushes backup jobs into a RabbitMQ queue, where they can be handled
+ * by worker nodes.
  */
 @ApplicationScoped
 public class RabbitMQJobManager extends AkkaJobManager {
-	private final Logger logger = LoggerFactory.getLogger(RabbitMQJobManager.class);
-	
-	@Inject
-	@Configuration(key="backmeup.message.queue.host")
-	private String mqHost;
-	
-	@Inject
-	@Configuration(key="backmeup.message.queue.name")
-	private String mqName;
-	
-	private Connection mqConnection;
-	
-	private Channel mqChannel;
-	
+    private final Logger logger = LoggerFactory.getLogger(RabbitMQJobManager.class);
+
+    @Inject
+    @Configuration(key = "backmeup.message.queue.host")
+    private String mqHost;
+
+    @Inject
+    @Configuration(key = "backmeup.message.queue.name")
+    private String mqName;
+
+    private Connection mqConnection;
+
+    private Channel mqChannel;
+
     @PostConstruct
-	@Override
-	public void start() {
-		super.start();
-		try {
-			init();
-		} catch (IOException e) {
-			throw new BackMeUpException(e);
-		}
-	}
-	
-	private void init() throws IOException {
-		// Setup connection to the message queue
-		ConnectionFactory factory = new ConnectionFactory();
-		factory.setHost(mqHost);
-		
-		mqConnection = factory.newConnection();
-		mqChannel = mqConnection.createChannel();
-		mqChannel.queueDeclare(mqName, false, false, false, null);		
-	}
+    @Override
+    public void start() {
+        super.start();
+        try {
+            init();
+        } catch (IOException e) {
+            throw new BackMeUpException(e);
+        }
+    }
+
+    private void init() throws IOException {
+        // Setup connection to the message queue
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setHost(this.mqHost);
+
+        this.mqConnection = factory.newConnection();
+        this.mqChannel = this.mqConnection.createChannel();
+        this.mqChannel.queueDeclare(this.mqName, false, false, false, null);
+    }
 
     @PreDestroy
-	@Override
-	public void shutdown() {
-		super.shutdown();
-		try {
-			if (mqChannel.isOpen()) {
-				mqChannel.close();
-			}
-			if (mqConnection.isOpen()) {
-				mqConnection.close();
-			}
-		} catch (IOException e) {
-			throw new RuntimeException(e);
-		}
-	}
-	
-	@Override
-	protected void runJob(BackupJob job) {
-		try {
-			// conn.beginOrJoin();
-			// we need a JPA-managed instance
-			// BackupJob job2 = dal.createBackupJobDao().findById(job.getId());
-			logger.info("Sending job to processing queue: " + job.getId());
-			// String json = JsonSerializer.serialize(job2);
-			// mqChannel.basicPublish("", mqName, null, json.getBytes());
-			byte[] bytes = longToBytes(job.getId());
-			mqChannel.basicPublish("", mqName, null, bytes);
-		} catch (IOException e) {
-			// Should only happen if message queue is down
-			logger.error("message queue down", e);
-			throw new RuntimeException(e);
-		} finally {
-			// conn.rollback();
-		}
-	}
+    @Override
+    public void shutdown() {
+        super.shutdown();
+        try {
+            if (this.mqChannel.isOpen()) {
+                this.mqChannel.close();
+            }
+            if (this.mqConnection.isOpen()) {
+                this.mqConnection.close();
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
 
-	private static byte[] longToBytes(long x) {
-		ByteBuffer buffer = ByteBuffer.allocate(Long.SIZE);    
-		buffer.putLong(0, x);
-		return buffer.array();
-	}
+    @Override
+    protected void runJob(BackupJob job) {
+        try {
+            // conn.beginOrJoin();
+            // we need a JPA-managed instance
+            // BackupJob job2 = dal.createBackupJobDao().findById(job.getId());
+            this.logger.info("Sending job to processing queue: " + job.getId() + " for userID: "
+                    + job.getUser().getUserId());
+            // String json = JsonSerializer.serialize(job2);
+            // mqChannel.basicPublish("", mqName, null, json.getBytes());
+            byte[] bytes = longToBytes(job.getId());
+            this.mqChannel.basicPublish("", this.mqName, null, bytes);
+        } catch (IOException e) {
+            // Should only happen if message queue is down
+            this.logger.error("message queue down", e);
+            throw new RuntimeException(e);
+        } finally {
+            // conn.rollback();
+        }
+    }
+
+    private static byte[] longToBytes(long x) {
+        ByteBuffer buffer = ByteBuffer.allocate(Long.SIZE);
+        buffer.putLong(0, x);
+        return buffer.array();
+    }
 }
