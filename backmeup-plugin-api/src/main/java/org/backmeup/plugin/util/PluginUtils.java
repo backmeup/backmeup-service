d7ae193faf1f38f8e2aaa0d283d958122e7ff31c
Wolfgang Eibner <we@x-net.at>
2015-01-19 18:21:31 +0100

    the other way round

--- backmeup-plugin-api/src/main/java/org/backmeup/plugin/util/PluginUtils.java
+++ backmeup-plugin-api/src/main/java/org/backmeup/plugin/util/PluginUtils.java
@@ -12,11 +12,11 @@ import java.util.LinkedHashMap;
 public class PluginUtils {
     public static class QueryParameters {
         private Map<String, List<String>> query_pairs;
-        
+
         public QueryParameters(Map<String, List<String>> query_pairs) {
             this.query_pairs = query_pairs;
         }
-        
+
         public String getParameter(String key) {
             List<String> values = this.query_pairs.get(key);
             if (values == null || values.size() < 1) {
@@ -24,7 +24,7 @@ public class PluginUtils {
             }
             return values.get(0);
         }
-        
+
         public List<String> getMultivalueParameter(String key) {
             List<String> values = this.query_pairs.get(key);
             if (values == null || values.size() < 1) {
@@ -32,31 +32,32 @@ public class PluginUtils {
             }
             return values;
         }
-        
+
     }
-    
+
     public static QueryParameters splitQuery(URL url) throws UnsupportedEncodingException {
+        return splitQuery(url.getQuery());
+    }
+
+    public static QueryParameters splitQuery(String queryParams) throws UnsupportedEncodingException {
+        if (queryParams == null) {
+            throw new NullPointerException();
+        }
+
         final Map<String, List<String>> query_pairs = new LinkedHashMap<String, List<String>>();
-        
-        for (String pair : url.getQuery().split("&")) {
+
+        for (String pair : queryParams.split("&")) {
             final int idx = pair.indexOf("=");
             final String key = idx > 0 ? URLDecoder.decode(pair.substring(0, idx), "UTF-8") : pair;
-            
+
             if (!query_pairs.containsKey(key)) {
                 query_pairs.put(key, new LinkedList<String>());
             }
-            
+
             final String value = idx > 0 && pair.length() > idx + 1 ? URLDecoder.decode(pair.substring(idx + 1), "UTF-8") : null;
             query_pairs.get(key).add(value);
         }
-        
+
         return new QueryParameters(query_pairs);
-    }
-    
-    public static QueryParameters splitQuery(String queryParams) throws UnsupportedEncodingException, MalformedURLException {
-        if(queryParams == null) {
-            throw new NullPointerException();
-        }
-        return splitQuery(new URL(queryParams));
     }
 }
