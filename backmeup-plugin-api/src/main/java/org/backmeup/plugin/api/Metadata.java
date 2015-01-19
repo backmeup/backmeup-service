1a25c8bd4a3740bb4fcdcba51b35fa61b80aca37
Andrew Lindley <andrew.lindley@ait.ac.at>
2015-01-19 12:06:34 +0100

    STORAGE_ALWAYS_ACCESSIBLE flag added to Metadata

--- backmeup-plugin-api/src/main/java/org/backmeup/plugin/api/Metadata.java
+++ backmeup-plugin-api/src/main/java/org/backmeup/plugin/api/Metadata.java
@@ -1,27 +1,32 @@
 package org.backmeup.plugin.api;
 
 public class Metadata {
-  /**
-   * Valid values are: DAILY, MONTHLY, WEEKLY
-   */
-  public final static String BACKUP_FREQUENCY="META_BACKUP_FREQUENCY";
-  
-  /**
-   * Datasink specific Metadata 
-   */
-  
-  /**
-   * The file size limit in MB (e.g. "100")
-   */
-  public final static String FILE_SIZE_LIMIT="META_FILE_SIZE_LIMIT";
-  
-  /**
-   * The users quota that is currently stored on a service in MB (e.g. "234.55") 
-   */
-  public final static String QUOTA="META_QUOTA";
-  
-  /**
-   * The maximal amount of data that can be stored on a service in MB (e.g. "2000"). 
-   */
-  public final static String QUOTA_LIMIT="META_QUOTA_LIMIT";
+    /**
+     * Valid values are: DAILY, MONTHLY, WEEKLY
+     */
+    public final static String BACKUP_FREQUENCY = "META_BACKUP_FREQUENCY";
+
+    /**
+     * Datasink specific Metadata
+     */
+
+    /**
+     * The file size limit in MB (e.g. "100")
+     */
+    public final static String FILE_SIZE_LIMIT = "META_FILE_SIZE_LIMIT";
+
+    /**
+     * The users quota that is currently stored on a service in MB (e.g. "234.55")
+     */
+    public final static String QUOTA = "META_QUOTA";
+
+    /**
+     * The maximal amount of data that can be stored on a service in MB (e.g. "2000").
+     */
+    public final static String QUOTA_LIMIT = "META_QUOTA_LIMIT";
+
+    /**
+     * Declares if a storage location is 'always on' i.e. the data accessible on demand. boolean flag True or False
+     */
+    public final static String STORAGE_ALWAYS_ACCESSIBLE = "META_STORAGE_ALWAYS_ACCESSIBLE";
 }
