2f87288b321954f08d73a8ee081aae0c7ab5c518
Andrew Lindley <andrew.lindley@ait.ac.at>
2015-01-23 00:36:33 +0100

    proposal on Datasink return value (javadoc)

--- backmeup-plugin-api/src/main/java/org/backmeup/plugin/api/connectors/Datasink.java
+++ backmeup-plugin-api/src/main/java/org/backmeup/plugin/api/connectors/Datasink.java
@@ -8,16 +8,29 @@ import org.backmeup.plugin.api.storage.StorageException;
 
 /**
  * 
- * The Datasink interface is the contract
- * for an upload of files to a certain datasink.
+ * The Datasink interface is the contract for an upload of files to a certain datasink.
  * 
  * 
  * @author fschoeppl
  *
  */
 public interface Datasink {
-	
-	public String upload(Properties accessData, Properties properties, List<String> options, Storage storage, Progressable progressor)
-		throws StorageException;
+
+    /**
+     * 
+     * @param accessData
+     * @param properties
+     * @param options
+     * @param storage
+     * @param progressor
+     * @return relative location for this backup upload without location prefix and the entire path up to the files e.g.
+     *         if the location is
+     *         http://localhost:8080/backmeup-storage-service/BMU_filegenerator_492_22_01_2015_21_14/file0.txt then
+     *         BMU_filegenerator_492_22_01_2015_21_14 is returned TODO need to clarify if this is the proper return
+     *         value for this String
+     * @throws StorageException
+     */
+    public String upload(Properties accessData, Properties properties, List<String> options, Storage storage,
+            Progressable progressor) throws StorageException;
 
 }
