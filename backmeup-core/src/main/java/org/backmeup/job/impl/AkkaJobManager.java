8f034157c2daae3fcb4eb95ea6a4eaba61302104
Sebastian Pimminger <sebastian.pimminger@gmail.com>
2015-02-02 16:16:53 +0100

    Replaced all tabs with spaces

--- backmeup-core/src/main/java/org/backmeup/job/impl/AkkaJobManager.java
+++ backmeup-core/src/main/java/org/backmeup/job/impl/AkkaJobManager.java
@@ -34,181 +34,181 @@ import akka.util.Duration;
  * @author Rainer Simon <rainer.simon@ait.ac.at>
  */
 public abstract class AkkaJobManager implements JobManager {
-	
-	private static final ActorSystem SYSTEM = ActorSystem.create();
-	
-	private static final Logger LOGGER = LoggerFactory.getLogger(AkkaJobManager.class);
-	
-	@Inject
-	protected Connection conn;
-	
-	@Inject
-	protected DataAccessLayer dal;
-	
-	@Inject
-	protected BackupLogic backupLogic;
-	
-	@Inject
-	private Keyserver keyserver;
-	
-	private BackupJobDao getBackupJobDao() {
-		return dal.createBackupJobDao();
-	}
-	
-	private BackupJob getBackUpJob(Long jobId) {
-		return getBackupJobDao().findById(jobId);
-	}
-	
-	@Override
-	public BackupJob createBackupJob(BackupJob backupJob) {
-		try {
-			conn.begin();
-			BackupJob job = backupLogic.createJob(backupJob);
-			conn.commit();
-			
-			// Queue new job immediately
-			queueJob(job);
-			return job;
-		} finally {
-			conn.rollback();
-		}
-	}
-	
-	@PostConstruct
-	public void start() {
-		// TODO only take N next recent ones (at least if allJobs has an
-		// excessive length)
-		try {
-			conn.begin();
-			List<BackupJob> storedJobs = getBackupJobDao().findAll();
-			conn.rollback();
-			for (BackupJob storedJob : storedJobs) {
-				queueJob(storedJob);
-			}
-		} finally {
-			conn.rollback();
-		}
-	}
-	
-	@Override
+
+    private static final ActorSystem SYSTEM = ActorSystem.create();
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(AkkaJobManager.class);
+
+    @Inject
+    protected Connection conn;
+
+    @Inject
+    protected DataAccessLayer dal;
+
+    @Inject
+    protected BackupLogic backupLogic;
+
+    @Inject
+    private Keyserver keyserver;
+
+    private BackupJobDao getBackupJobDao() {
+        return dal.createBackupJobDao();
+    }
+
+    private BackupJob getBackUpJob(Long jobId) {
+        return getBackupJobDao().findById(jobId);
+    }
+
+    @Override
+    public BackupJob createBackupJob(BackupJob backupJob) {
+        try {
+            conn.begin();
+            BackupJob job = backupLogic.createJob(backupJob);
+            conn.commit();
+
+            // Queue new job immediately
+            queueJob(job);
+            return job;
+        } finally {
+            conn.rollback();
+        }
+    }
+
+    @PostConstruct
+    public void start() {
+        // TODO only take N next recent ones (at least if allJobs has an
+        // excessive length)
+        try {
+            conn.begin();
+            List<BackupJob> storedJobs = getBackupJobDao().findAll();
+            conn.rollback();
+            for (BackupJob storedJob : storedJobs) {
+                queueJob(storedJob);
+            }
+        } finally {
+            conn.rollback();
+        }
+    }
+
+    @Override
     public void runBackUpJob(BackupJob job) {
-		queueJob(job);
-	}
+        queueJob(job);
+    }
 
-	@PreDestroy
-	public void shutdown() {
-		// Shutdown system component
-		SYSTEM.shutdown();
-		SYSTEM.awaitTermination();
-	}
+    @PreDestroy
+    public void shutdown() {
+        // Shutdown system component
+        SYSTEM.shutdown();
+        SYSTEM.awaitTermination();
+    }
 
-	protected abstract void runJob(BackupJob job);
+    protected abstract void runJob(BackupJob job);
 
-	// Don't call this method within a database transaction!
-	private void queueJob(BackupJob job) {
-		try {
-			// Compute next job execution time
-			long currentTime = new Date().getTime();
-			long executeIn = job.getStart().getTime() - currentTime;
-			if (job.getNextExecutionTime() != null) {
-				executeIn = job.getNextExecutionTime().getTime() - currentTime;
-			}
+    // Don't call this method within a database transaction!
+    private void queueJob(BackupJob job) {
+        try {
+            // Compute next job execution time
+            long currentTime = new Date().getTime();
+            long executeIn = job.getStart().getTime() - currentTime;
+            if (job.getNextExecutionTime() != null) {
+                executeIn = job.getNextExecutionTime().getTime() - currentTime;
+            }
 
-			// If job execution was scheduled for within the past 5 mins, still
-			// schedule now...
-			if (executeIn >= -300000 && executeIn < 0) {
-				executeIn = 0;
-			}
+            // If job execution was scheduled for within the past 5 mins, still
+            // schedule now...
+            if (executeIn >= -300000 && executeIn < 0) {
+                executeIn = 0;
+            }
 
-			// ...otherwise, schedule on the next occasion defined by .getStart
-			// and .getDelay
-			if (executeIn < 0) {
-				executeIn += Math.ceil((double) Math.abs(executeIn) / (double) job.getDelay()) * job.getDelay();
-				conn.begin();
-				job = getBackUpJob(job.getId());
-				// TODO we need to update these jobs' tokens - but where do we
-				// get keyRing password from?
-				job.getToken().setBackupdate(currentTime + executeIn);
+            // ...otherwise, schedule on the next occasion defined by .getStart
+            // and .getDelay
+            if (executeIn < 0) {
+                executeIn += Math.ceil((double) Math.abs(executeIn) / (double) job.getDelay()) * job.getDelay();
+                conn.begin();
+                job = getBackUpJob(job.getId());
+                // TODO we need to update these jobs' tokens - but where do we
+                // get keyRing password from?
+                job.getToken().setBackupdate(currentTime + executeIn);
 
-				// get access data + new token for next access
-				AuthDataResult authenticationData = keyserver.getData(job.getToken());
+                // get access data + new token for next access
+                AuthDataResult authenticationData = keyserver.getData(job.getToken());
 
-				// the token for the next getData call
-				Token newToken = authenticationData.getNewToken();
-				job.setToken(newToken);
-				conn.commit();
-			}
+                // the token for the next getData call
+                Token newToken = authenticationData.getNewToken();
+                job.setToken(newToken);
+                conn.commit();
+            }
 
-			// Add the scheduler id to the job. If the job gets executed it will
-			// be possible to check if this job is still valid
-			conn.begin();
-			job = getBackUpJob(job.getId());
-			UUID schedulerID = UUID.randomUUID();
-			job.setValidScheduleID(schedulerID);
-			conn.commit();
+            // Add the scheduler id to the job. If the job gets executed it will
+            // be possible to check if this job is still valid
+            conn.begin();
+            job = getBackUpJob(job.getId());
+            UUID schedulerID = UUID.randomUUID();
+            job.setValidScheduleID(schedulerID);
+            conn.commit();
 
-			// We can use the 'cancellable' to terminate later on
-			SYSTEM.scheduler().scheduleOnce(
-			        // Initial delay
-					Duration.create(executeIn, TimeUnit.MILLISECONDS),
-					new RunAndReschedule(job, dal, schedulerID));
+            // We can use the 'cancellable' to terminate later on
+            SYSTEM.scheduler().scheduleOnce(
+                    // Initial delay
+                    Duration.create(executeIn, TimeUnit.MILLISECONDS),
+                    new RunAndReschedule(job, dal, schedulerID));
 
-		} catch (Exception e) {
-			// TODO there must be error handling defined in the JobManager!^
-			LOGGER.error("Error during startup", e);
-			// throw new BackMeUpException(e);
-		} finally {
-			conn.rollback();
-		}
-	}
-	
-	private class RunAndReschedule implements Runnable {
-		private final Logger logger = LoggerFactory.getLogger(RunAndReschedule.class);
+        } catch (Exception e) {
+            // TODO there must be error handling defined in the JobManager!^
+            LOGGER.error("Error during startup", e);
+            // throw new BackMeUpException(e);
+        } finally {
+            conn.rollback();
+        }
+    }
 
-		private final BackupJob job;
-		private final DataAccessLayer dal;
-		private final UUID schedulerID;
+    private class RunAndReschedule implements Runnable {
+        private final Logger logger = LoggerFactory.getLogger(RunAndReschedule.class);
 
-		RunAndReschedule(BackupJob job, DataAccessLayer dal, UUID schedulerID) {
-			this.job = job;
-			this.dal = dal;
-			this.schedulerID = schedulerID;
-		}
+        private final BackupJob job;
+        private final DataAccessLayer dal;
+        private final UUID schedulerID;
 
-		@Override
-		public void run() {
-			// check if the scheduler is still valid. If not a new scheduler was
-			// created and this one should not be executed
-			if (job.getValidScheduleID().compareTo(schedulerID) != 0) {
-				return;
-			}
+        RunAndReschedule(BackupJob job, DataAccessLayer dal, UUID schedulerID) {
+            this.job = job;
+            this.dal = dal;
+            this.schedulerID = schedulerID;
+        }
 
-			// Run the job
-			if (!job.isOnHold()) {
-				runJob(job);
-			}
+        @Override
+        public void run() {
+            // check if the scheduler is still valid. If not a new scheduler was
+            // created and this one should not be executed
+            if (job.getValidScheduleID().compareTo(schedulerID) != 0) {
+                return;
+            }
 
-			// Reschedule if it's still in the DB
-			try {
-				conn.beginOrJoin();
+            // Run the job
+            if (!job.isOnHold()) {
+                runJob(job);
+            }
 
-				BackupJob nextJob = dal.createBackupJobDao().findById(job.getId());
-				if (nextJob != null && nextJob.isReschedule()) {
-					logger.debug("Rescheduling job for execution in "+ job.getDelay() + "ms");
-					Date execTime = new Date(new Date().getTime() + job.getDelay());
-					nextJob.setNextExecutionTime(execTime);
-					SYSTEM.scheduler().scheduleOnce(
-							Duration.create(job.getDelay(),
-									TimeUnit.MILLISECONDS),
-							new RunAndReschedule(job, dal, schedulerID));
-					// store the next execution time
-					conn.commit();
-				} else {
-					logger.debug("Job deleted in the mean time - no re-scheduling.");
-				}
-			} finally {
-				conn.rollback();
-			}
-		}
-	}
+            // Reschedule if it's still in the DB
+            try {
+                conn.beginOrJoin();
+
+                BackupJob nextJob = dal.createBackupJobDao().findById(job.getId());
+                if (nextJob != null && nextJob.isReschedule()) {
+                    logger.debug("Rescheduling job for execution in "+ job.getDelay() + "ms");
+                    Date execTime = new Date(new Date().getTime() + job.getDelay());
+                    nextJob.setNextExecutionTime(execTime);
+                    SYSTEM.scheduler().scheduleOnce(
+                            Duration.create(job.getDelay(),
+                                    TimeUnit.MILLISECONDS),
+                                    new RunAndReschedule(job, dal, schedulerID));
+                    // store the next execution time
+                    conn.commit();
+                } else {
+                    logger.debug("Job deleted in the mean time - no re-scheduling.");
+                }
+            } finally {
+                conn.rollback();
+            }
+        }
+    }
 }
