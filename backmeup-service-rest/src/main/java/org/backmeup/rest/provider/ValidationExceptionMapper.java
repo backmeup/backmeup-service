79f2cbf36f7f40e09d222ee495fd9b66f37b8f29
Wolfgang Eibner <we@x-net.at>
2015-02-19 17:53:09 +0100

    Also add general exception info at ValidationException with ConfigException type and change logic; refs #186.

--- backmeup-service-rest/src/main/java/org/backmeup/rest/provider/ValidationExceptionMapper.java
+++ backmeup-service-rest/src/main/java/org/backmeup/rest/provider/ValidationExceptionMapper.java
@@ -1,26 +1,26 @@
 package org.backmeup.rest.provider;
 
+import java.util.Map;
+
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.Response.Status;
 import javax.ws.rs.ext.ExceptionMapper;
-import static org.backmeup.rest.provider.BackMeUpExceptionMapper.mapExceptionInformation;
 
 import org.backmeup.model.exceptions.ValidationException;
 
 public class ValidationExceptionMapper implements ExceptionMapper<ValidationException> {    
     public Response toResponse(ValidationException exception)  {
-        switch(exception.getType()) {
-        case ConfigException:
-            if (exception.getNotes() != null && exception.getNotes().hasEntries()) {
-                return Response.status(Status.BAD_REQUEST).entity(exception.getNotes()).build();
-            } else {
-                return Response.status(Status.BAD_REQUEST).entity(mapExceptionInformation(exception)).build();
-            }
-        case APIException:
-        case AuthException: 
-            return Response.status(Status.BAD_REQUEST).entity(mapExceptionInformation(exception)).build();
-        default:
-            return Response.status(Status.INTERNAL_SERVER_ERROR).entity(mapExceptionInformation(exception)).build();
+        return Response.status(Status.BAD_REQUEST).entity(mapExceptionInformation(exception)).build();
+    }
+    
+    public static Map<String, Object> mapExceptionInformation(ValidationException e) {
+        Map<String, Object> info = BackMeUpExceptionMapper.mapExceptionInformation(e);
+        
+        info.put("type", e.getType());
+        if (e.getNotes() != null && e.getNotes().hasEntries()) {
+            info.put("validationEntries", e.getNotes());
         }
+        
+        return info;
     }
 }
