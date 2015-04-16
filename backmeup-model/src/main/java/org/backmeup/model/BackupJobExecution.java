3a2b26663770ef1b22997574fcfce9b6dd6325ba
Sebastian Pimminger <sebastian.pimminger@gmail.com>
2015-04-16 11:45:30 +0200

    Fixed wrong mapping annotation

--- backmeup-model/src/main/java/org/backmeup/model/BackupJobExecution.java
+++ backmeup-model/src/main/java/org/backmeup/model/BackupJobExecution.java
@@ -14,6 +14,7 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
@@ -59,7 +60,7 @@ public class BackupJobExecution {
     @ManyToOne(cascade = CascadeType.REFRESH, fetch = FetchType.EAGER)
     private Profile sourceProfile;
     
-    @ManyToOne(cascade = CascadeType.REFRESH, fetch = FetchType.EAGER)
+    @OneToMany(cascade = CascadeType.REFRESH, fetch = FetchType.EAGER)
     private List<Profile> actionProfiles = new ArrayList<>();
     
     @ManyToOne(cascade = CascadeType.REFRESH, fetch = FetchType.EAGER)
