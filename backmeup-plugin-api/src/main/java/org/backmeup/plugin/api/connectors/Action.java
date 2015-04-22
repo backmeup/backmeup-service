b328e5d05a5cf41db2d4356bc29d3345d2df7f1f
Sebastian Pimminger <sebastian.pimminger@gmail.com>
2015-04-22 16:40:01 +0200

    Now we operate on backupjob executions

--- backmeup-plugin-api/src/main/java/org/backmeup/plugin/api/connectors/Action.java
+++ backmeup-plugin-api/src/main/java/org/backmeup/plugin/api/connectors/Action.java
@@ -3,12 +3,12 @@ package org.backmeup.plugin.api.connectors;
 import java.util.List;
 import java.util.Map;
 
-import org.backmeup.model.dto.BackupJobDTO;
+import org.backmeup.model.dto.BackupJobExecutionDTO;
 import org.backmeup.plugin.api.storage.Storage;
 
 public interface Action {
 
     public void doAction(Map<String, String> authData, Map<String, String> properties, List<String> options,
-            Storage storage, BackupJobDTO job, Progressable progressor)
+            Storage storage, BackupJobExecutionDTO job, Progressable progressor)
             throws ActionException;
 }
