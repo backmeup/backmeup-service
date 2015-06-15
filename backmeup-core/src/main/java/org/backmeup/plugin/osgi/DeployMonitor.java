51c445b04486c569037a224beb55138253069438
Sebastian Pimminger <sebastian.pimminger@gmail.com>
2015-06-15 10:02:38 +0200

    Code formatting and correct indentation

--- backmeup-core/src/main/java/org/backmeup/plugin/osgi/DeployMonitor.java
+++ backmeup-core/src/main/java/org/backmeup/plugin/osgi/DeployMonitor.java
@@ -25,123 +25,123 @@ import org.slf4j.LoggerFactory;
  * @author fschoeppl
  */
 public class DeployMonitor implements Runnable {
-  private static final Logger LOGGER = LoggerFactory.getLogger(DeployMonitor.class);
+    private static final Logger LOGGER = LoggerFactory.getLogger(DeployMonitor.class);
 
-  private final Map<File, Bundle> deployed = new HashMap<>();
-  private final List<Bundle> newlyInstalledBundles = new LinkedList<>();
-  private final List<File> toBeRemovedBundles = new LinkedList<>();
-  private final BundleContext context;
-  private final File deploymentDirectory;
-  private final Object monitor = new Object();
-  
-  private ScheduledExecutorService executor;
-  private boolean firstRun = false;
+    private final Map<File, Bundle> deployed = new HashMap<>();
+    private final List<Bundle> newlyInstalledBundles = new LinkedList<>();
+    private final List<File> toBeRemovedBundles = new LinkedList<>();
+    private final BundleContext context;
+    private final File deploymentDirectory;
+    private final Object monitor = new Object();
 
-  public DeployMonitor(BundleContext context, File deploymentDirectory) {
-    this.context = context;
-    this.deploymentDirectory = deploymentDirectory;
-  }
+    private ScheduledExecutorService executor;
+    private boolean firstRun = false;
 
-  public void start() {
-    if (executor != null) {
-      stop();
+    public DeployMonitor(BundleContext context, File deploymentDirectory) {
+        this.context = context;
+        this.deploymentDirectory = deploymentDirectory;
     }
-    executor = Executors.newScheduledThreadPool(1);
-    executor.scheduleAtFixedRate(this, 0, 5, TimeUnit.SECONDS);
 
-  }
-
-  public void waitForInitialRun() {
-    try {
-        synchronized (monitor) {
-            while (!firstRun) {
-                monitor.wait();
-            }
+    public void start() {
+        if (executor != null) {
+            stop();
         }
-    } catch (InterruptedException e) {
-    	LOGGER.error("", e);
-    }
-  }
+        executor = Executors.newScheduledThreadPool(1);
+        executor.scheduleAtFixedRate(this, 0, 5, TimeUnit.SECONDS);
 
-  public void stop() {
-    executor.shutdown();
-    executor.shutdownNow();
-    try {
-      executor.awaitTermination(1, TimeUnit.MINUTES);
-      LOGGER.error("Awaited termination of executor!");
-      synchronized (monitor) {
-        firstRun = true;
-        monitor.notifyAll();
-      }
-    } catch (InterruptedException e) {
-    	LOGGER.error("", e);
     }
-  }
 
-  @Override
-  public void run() {
+    public void waitForInitialRun() {
+        try {
+            synchronized (monitor) {
+                while (!firstRun) {
+                    monitor.wait();
+                }
+            }
+        } catch (InterruptedException e) {
+            LOGGER.error("", e);
+        }
+    }
+
+    public void stop() {
+        executor.shutdown();
+        executor.shutdownNow();
+        try {
+            executor.awaitTermination(1, TimeUnit.MINUTES);
+            LOGGER.error("Awaited termination of executor!");
+            synchronized (monitor) {
+                firstRun = true;
+                monitor.notifyAll();
+            }
+        } catch (InterruptedException e) {
+            LOGGER.error("", e);
+        }
+    }
+
+    @Override
+    public void run() {
         if (!deploymentDirectory.exists() && !deploymentDirectory.mkdirs()) {
             throw new BackMeUpException("Cannot create deployment directory");
         }
 
-    installBundles();
+        installBundles();
 
-    removeBundles();
-   
-    if (!firstRun) {        
-      synchronized(monitor) {
-        firstRun = true;
-        monitor.notifyAll();
-      }
-    }
-  }
+        removeBundles();
 
-private void removeBundles() {
-    for (File f : deployed.keySet()) {
-      if (!f.exists() || deployed.get(f).getState() == Bundle.UNINSTALLED) {
-        try {
-          Bundle b = deployed.get(f);
-          if (b.getState() == Bundle.ACTIVE) {
-            b.stop();
-          }
-          if (b.getState() != Bundle.UNINSTALLED) {
-            b.uninstall();
-          }
-          toBeRemovedBundles.add(f);
-        } catch (Exception e) {
-          LOGGER.error("", e);
+        if (!firstRun) {        
+            synchronized(monitor) {
+                firstRun = true;
+                monitor.notifyAll();
+            }
         }
-      }
     }
 
-    for (File toBeRemovedFile : toBeRemovedBundles) {
-      deployed.remove(toBeRemovedFile);
-    }
-    toBeRemovedBundles.clear();
-}
-
-private void installBundles() {
-    for (File f : deploymentDirectory.listFiles()) {
-      if ((f.getName().endsWith(".jar")) && (!deployed.containsKey(f))) {
-          try {
-            Bundle b = context.installBundle("file:" + f.getAbsolutePath());
-            deployed.put(f, b);
-            newlyInstalledBundles.add(b);
-          } catch (Exception e) {
-        	  LOGGER.error("", e);
-          }
-      }
-    }
-
-    for (Bundle newlyInstalledBundle : newlyInstalledBundles) {
-      try {
-        if (newlyInstalledBundle.getHeaders().get(Constants.FRAGMENT_HOST) == null) {
-          newlyInstalledBundle.start();
+    private void removeBundles() {
+        for (File f : deployed.keySet()) {
+            if (!f.exists() || deployed.get(f).getState() == Bundle.UNINSTALLED) {
+                try {
+                    Bundle b = deployed.get(f);
+                    if (b.getState() == Bundle.ACTIVE) {
+                        b.stop();
+                    }
+                    if (b.getState() != Bundle.UNINSTALLED) {
+                        b.uninstall();
+                    }
+                    toBeRemovedBundles.add(f);
+                } catch (Exception e) {
+                    LOGGER.error("", e);
+                }
+            }
         }
-      } catch (Exception e) {
-    	  LOGGER.error("", e);
-      }
+
+        for (File toBeRemovedFile : toBeRemovedBundles) {
+            deployed.remove(toBeRemovedFile);
+        }
+        toBeRemovedBundles.clear();
     }
-    newlyInstalledBundles.clear();
-}
+
+    private void installBundles() {
+        for (File f : deploymentDirectory.listFiles()) {
+            if ((f.getName().endsWith(".jar")) && (!deployed.containsKey(f))) {
+                try {
+                    Bundle b = context.installBundle("file:" + f.getAbsolutePath());
+                    deployed.put(f, b);
+                    newlyInstalledBundles.add(b);
+                } catch (Exception e) {
+                    LOGGER.error("", e);
+                }
+            }
+        }
+
+        for (Bundle newlyInstalledBundle : newlyInstalledBundles) {
+            try {
+                if (newlyInstalledBundle.getHeaders().get(Constants.FRAGMENT_HOST) == null) {
+                    newlyInstalledBundle.start();
+                }
+            } catch (Exception e) {
+                LOGGER.error("", e);
+            }
+        }
+        newlyInstalledBundles.clear();
+    }
 }
