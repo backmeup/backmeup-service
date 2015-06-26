e37a93df6d9b62906083c645157a1af0af30ca9e
Sebastian Pimminger <sebastian.pimminger@gmail.com>
2015-06-26 21:46:55 +0200

    Reordered methods for better understanding

--- backmeup-plugin-infrastructure/src/main/java/org/backmeup/plugin/infrastructure/PluginManager.java
+++ backmeup-plugin-infrastructure/src/main/java/org/backmeup/plugin/infrastructure/PluginManager.java
@@ -77,6 +77,7 @@ public class PluginManager {
 
     private DeploymentMonitor deploymentMonitor;
 
+    // Constructors -----------------------------------------------------------
 
     public PluginManager() {
 
@@ -89,6 +90,8 @@ public class PluginManager {
         this.exportedPackages = exportedPackages;
         this.started = false;
     }
+
+    // Lifecycle methods ------------------------------------------------------
 
     public void startup() {
         if (!started) {
@@ -109,181 +112,6 @@ public class PluginManager {
         deploymentMonitor.waitForInitialRun();
     }
 
-    private void startDeploymentMonitor() {
-        this.deploymentMonitor = new DeploymentMonitor(bundleContext(), deploymentDirectory);
-        this.deploymentMonitor.start();
-    }
-
-    public static boolean deleteDir(File dir) {
-        if (dir.isDirectory()) {
-            String[] children = dir.list();
-            for (String element : children) {
-                boolean success = deleteDir(new File(dir, element));
-                if (!success) {
-                    return false;
-                }
-            }
-        }
-        // The directory is now empty so delete it
-        return dir.delete();
-    }
-
-    public void initOSGiFramework() {
-        try {
-            FrameworkFactory factory = new FrameworkFactory();
-            if (temporaryDirectory.exists()) {
-                deleteDir(temporaryDirectory);
-            }
-            
-            Map<String, String> config = new HashMap<>();
-            config.put(Constants.FRAMEWORK_SYSTEMPACKAGES_EXTRA, exportedPackages);
-            config.put(Constants.FRAMEWORK_STORAGE, temporaryDirectory.getAbsolutePath());
-            config.put(Constants.FRAMEWORK_STORAGE_CLEAN, "true");
-            config.put(Constants.FRAMEWORK_BUNDLE_PARENT, Constants.FRAMEWORK_BUNDLE_PARENT_FRAMEWORK);
-            config.put(Constants.FRAMEWORK_BOOTDELEGATION, exportedPackages);
-
-            LOGGER.debug("EXPORTED PACKAGES: " + exportedPackages);
-
-            osgiFramework = factory.newFramework(config);
-            osgiFramework.start();
-        } catch (Exception e) {
-            LOGGER.error("", e);
-            throw new BackMeUpException(e);
-        }
-    }
-
-    public void stopOSGiFramework() {
-        try {
-            osgiFramework.stop();
-            osgiFramework.waitForStop(0);
-            LOGGER.debug("OsgiFramework stopped.");
-        } catch (InterruptedException e) {
-            LOGGER.error("", e);
-        } catch (BundleException e) {
-            LOGGER.error("", e);
-        }
-    }
-
-    public BundleContext bundleContext() {
-        return osgiFramework.getBundleContext();
-    }
-
-    public <T> T service(final Class<T> service) {
-        return service(service, null);
-    }
-
-    private <T> ServiceReference getReference(final Class<T> service, final String pluginId) {
-        ServiceReference ref = null;
-        if (pluginId == null) {
-            ref = bundleContext().getServiceReference(service.getName());
-        } else {
-            ServiceReference[] refs;
-            String filter = String.format(PLUGIN_FILTER_FORMAT, pluginId);
-            try {
-                refs = bundleContext().getServiceReferences(service.getName(), filter);
-            } catch (InvalidSyntaxException e) {
-                throw new IllegalArgumentException(String.format("The filter '%s' is mallformed.", filter), e);
-            }
-            if (refs != null && refs.length > 0) {
-                ref = refs[0];
-            }
-        }
-        return ref;
-    }
-
-    @SuppressWarnings("unchecked")
-    public <T> T service(final Class<T> service, final String pluginId) {
-        ServiceReference ref = getReference(service, pluginId);
-        if (ref == null) {
-            throw new PluginUnavailableException(pluginId);
-        }
-        bundleContext().ungetService(ref);
-        return (T) Proxy.newProxyInstance(PluginManager.class.getClassLoader(),
-                new Class[] { service }, new InvocationHandler() {
-
-            @Override
-            public Object invoke(Object o, Method method, Object[] os) throws Throwable {
-                ServiceReference serviceRef = getReference(service, pluginId);
-                if (serviceRef == null) {
-                    throw new PluginUnavailableException(pluginId);
-                }
-                Object instance = bundleContext().getService(serviceRef);
-                Object ret = null;
-                try {
-                    ret = method.invoke(instance, os);
-                } catch (Exception e) {
-                    throw new PluginException(pluginId, "An exception occured during execution of the method " + method.getName(), e);
-                } finally {
-                    bundleContext().ungetService(serviceRef);
-                }
-                return ret;
-            }
-        });
-    }
-
-    private static class SpecialInvocationHandler implements InvocationHandler {
-        private final ServiceReference reference;
-        private final BundleContext context;
-
-        public SpecialInvocationHandler(BundleContext context, ServiceReference reference) {
-            this.reference = reference;
-            this.context = context;
-        }
-
-        @Override
-        public Object invoke(Object o, Method method, Object[] os) throws Throwable {
-            ServiceReference ref = reference;
-            Object ret = null;
-            @SuppressWarnings("unchecked")
-            Object instance = context.getService(ref);
-            if (instance == null) {
-                LOGGER.error(
-                        "FATAL ERROR:\n\tCalling the method \"{}\" of a null-instance from bundle \"{}\"; getService returned null!\n",
-                        method.getName(), ref.getBundle().getSymbolicName());
-            }
-            try {
-                boolean acc = method.isAccessible();
-                method.setAccessible(true);
-
-                if (os == null) {
-                    ret = method.invoke(instance);
-                } else {
-                    ret = method.invoke(instance, os);
-                }
-                method.setAccessible(acc);
-            } finally {
-                context.ungetService(ref);
-            }
-            return ret;
-        }
-    }
-
-    @SuppressWarnings("unchecked")
-    public <T> Iterable<T> services(final Class<T> service, final String filter) {
-        return new Iterable<T>() {
-
-            @Override
-            public Iterator<T> iterator() {
-                try {
-                    List<T> services = new ArrayList<>();
-                    ServiceReference[] refs = bundleContext().getServiceReferences(service.getName(), filter);
-                    if (refs != null) {
-                        for (ServiceReference s : refs) {
-                            services.add((T) Proxy.newProxyInstance(
-                                    PluginManager.class.getClassLoader(),
-                                    new Class[] { service },
-                                    new SpecialInvocationHandler(bundleContext(), s)));
-                        }
-                    }
-                    return services.iterator();
-                } catch (Exception e) {
-                    throw new BackMeUpException(e);
-                }
-            }
-
-        };
-    }
-
     public void shutdown() {
         if (started) {
             LOGGER.debug("Shutting down PluginImpl!");
@@ -293,18 +121,7 @@ public class PluginManager {
         }
     }
 
-    protected List<PluginDescribable> getDescribableForType(PluginType... pluginTypes) {
-        List<PluginType> lpluginTypes = Arrays.asList(pluginTypes);
-        Iterable<PluginDescribable> descs = services(
-                PluginDescribable.class, null);
-        List<PluginDescribable> result = new ArrayList<>();
-        for (PluginDescribable d : descs) {
-            if (lpluginTypes.contains(d.getType())) {
-                result.add(d);
-            }
-        }
-        return result;
-    }
+    // Public methods ---------------------------------------------------------
 
     public boolean isPluginAvailable(String pluginId) {
         ServiceReference ref = getReference(PluginDescribable.class, pluginId);
@@ -366,5 +183,199 @@ public class PluginManager {
 
     public Validationable getValidator(String sourceSinkId) {
         return service(Validationable.class, sourceSinkId);
+    }
+
+    public <T> T service(final Class<T> service) {
+        return service(service, null);
+    }
+
+    @SuppressWarnings("unchecked")
+    public <T> T service(final Class<T> service, final String pluginId) {
+        ServiceReference ref = getReference(service, pluginId);
+        if (ref == null) {
+            throw new PluginUnavailableException(pluginId);
+        }
+        getBundleContext().ungetService(ref);
+        return (T) Proxy.newProxyInstance(PluginManager.class.getClassLoader(),
+                new Class[] { service }, new InvocationHandler() {
+
+            @Override
+            public Object invoke(Object o, Method method, Object[] os) throws Throwable {
+                ServiceReference serviceRef = getReference(service, pluginId);
+                if (serviceRef == null) {
+                    throw new PluginUnavailableException(pluginId);
+                }
+                Object instance = getBundleContext().getService(serviceRef);
+                Object ret = null;
+                try {
+                    ret = method.invoke(instance, os);
+                } catch (Exception e) {
+                    throw new PluginException(pluginId, "An exception occured during execution of the method " + method.getName(), e);
+                } finally {
+                    getBundleContext().ungetService(serviceRef);
+                }
+                return ret;
+            }
+        });
+    }
+
+    @SuppressWarnings("unchecked")
+    public <T> Iterable<T> services(final Class<T> service, final String filter) {
+        return new Iterable<T>() {
+
+            @Override
+            public Iterator<T> iterator() {
+                try {
+                    List<T> services = new ArrayList<>();
+                    ServiceReference[] refs = getBundleContext().getServiceReferences(service.getName(), filter);
+                    if (refs != null) {
+                        for (ServiceReference s : refs) {
+                            services.add((T) Proxy.newProxyInstance(
+                                    PluginManager.class.getClassLoader(),
+                                    new Class[] { service },
+                                    new SpecialInvocationHandler(getBundleContext(), s)));
+                        }
+                    }
+                    return services.iterator();
+                } catch (Exception e) {
+                    throw new BackMeUpException(e);
+                }
+            }
+
+        };
+    }
+
+    protected List<PluginDescribable> getDescribableForType(PluginType... pluginTypes) {
+        List<PluginType> lpluginTypes = Arrays.asList(pluginTypes);
+        Iterable<PluginDescribable> descs = services(
+                PluginDescribable.class, null);
+        List<PluginDescribable> result = new ArrayList<>();
+        for (PluginDescribable d : descs) {
+            if (lpluginTypes.contains(d.getType())) {
+                result.add(d);
+            }
+        }
+        return result;
+    }
+
+    // Private methods --------------------------------------------------------
+
+    private BundleContext getBundleContext() {
+        return osgiFramework.getBundleContext();
+    }
+
+    private <T> ServiceReference getReference(final Class<T> service, final String pluginId) {
+        ServiceReference ref = null;
+        if (pluginId == null) {
+            ref = getBundleContext().getServiceReference(service.getName());
+        } else {
+            ServiceReference[] refs;
+            String filter = String.format(PLUGIN_FILTER_FORMAT, pluginId);
+            try {
+                refs = getBundleContext().getServiceReferences(service.getName(), filter);
+            } catch (InvalidSyntaxException e) {
+                throw new IllegalArgumentException(String.format("The filter '%s' is mallformed.", filter), e);
+            }
+            if (refs != null && refs.length > 0) {
+                ref = refs[0];
+            }
+        }
+        return ref;
+    }
+
+    public static boolean deleteDir(File dir) {
+        if (dir.isDirectory()) {
+            String[] children = dir.list();
+            for (String element : children) {
+                boolean success = deleteDir(new File(dir, element));
+                if (!success) {
+                    return false;
+                }
+            }
+        }
+        // The directory is now empty so delete it
+        return dir.delete();
+    }
+
+    // Private lifecycle methods ----------------------------------------------
+
+    private void initOSGiFramework() {
+        try {
+            FrameworkFactory factory = new FrameworkFactory();
+            if (temporaryDirectory.exists()) {
+                deleteDir(temporaryDirectory);
+            }
+
+            Map<String, String> config = new HashMap<>();
+            config.put(Constants.FRAMEWORK_SYSTEMPACKAGES_EXTRA, exportedPackages);
+            config.put(Constants.FRAMEWORK_STORAGE, temporaryDirectory.getAbsolutePath());
+            config.put(Constants.FRAMEWORK_STORAGE_CLEAN, "true");
+            config.put(Constants.FRAMEWORK_BUNDLE_PARENT, Constants.FRAMEWORK_BUNDLE_PARENT_FRAMEWORK);
+            config.put(Constants.FRAMEWORK_BOOTDELEGATION, exportedPackages);
+
+            LOGGER.debug("EXPORTED PACKAGES: " + exportedPackages);
+
+            osgiFramework = factory.newFramework(config);
+            osgiFramework.start();
+        } catch (Exception e) {
+            LOGGER.error("", e);
+            throw new BackMeUpException(e);
+        }
+    }
+
+    private void startDeploymentMonitor() {
+        this.deploymentMonitor = new DeploymentMonitor(getBundleContext(), deploymentDirectory);
+        this.deploymentMonitor.start();
+    }
+
+    private void stopOSGiFramework() {
+        try {
+            osgiFramework.stop();
+            osgiFramework.waitForStop(0);
+            LOGGER.debug("OsgiFramework stopped.");
+        } catch (InterruptedException e) {
+            LOGGER.error("", e);
+        } catch (BundleException e) {
+            LOGGER.error("", e);
+        }
+    }
+
+    // Private classes --------------------------------------------------------
+
+    private static class SpecialInvocationHandler implements InvocationHandler {
+        private final ServiceReference reference;
+        private final BundleContext context;
+
+        public SpecialInvocationHandler(BundleContext context, ServiceReference reference) {
+            this.reference = reference;
+            this.context = context;
+        }
+
+        @Override
+        public Object invoke(Object o, Method method, Object[] os) throws Throwable {
+            ServiceReference ref = reference;
+            Object ret = null;
+            @SuppressWarnings("unchecked")
+            Object instance = context.getService(ref);
+            if (instance == null) {
+                LOGGER.error(
+                        "FATAL ERROR:\n\tCalling the method \"{}\" of a null-instance from bundle \"{}\"; getService returned null!\n",
+                        method.getName(), ref.getBundle().getSymbolicName());
+            }
+            try {
+                boolean acc = method.isAccessible();
+                method.setAccessible(true);
+
+                if (os == null) {
+                    ret = method.invoke(instance);
+                } else {
+                    ret = method.invoke(instance, os);
+                }
+                method.setAccessible(acc);
+            } finally {
+                context.ungetService(ref);
+            }
+            return ret;
+        }
     }
 }
