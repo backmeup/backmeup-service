b8b10da4a2daf388240cf0bb9c12d41da8a20794
Sebastian Pimminger <sebastian.pimminger@gmail.com>
2015-07-17 13:59:51 +0200

    Set worker Id for received metrics Note: currently this is a dummy id

--- backmeup-service-rest/src/main/java/org/backmeup/rest/resources/Workers.java
+++ backmeup-service-rest/src/main/java/org/backmeup/rest/resources/Workers.java
@@ -9,17 +9,23 @@ import javax.ws.rs.POST;
 import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.SecurityContext;
 
+import org.backmeup.model.BackMeUpUser;
 import org.backmeup.model.WorkerInfo;
 import org.backmeup.model.WorkerMetric;
 import org.backmeup.model.dto.WorkerConfigDTO;
 import org.backmeup.model.dto.WorkerInfoDTO;
 import org.backmeup.model.dto.WorkerMetricDTO;
+import org.backmeup.rest.auth.BackmeupPrincipal;
 
 @Path("/workers")
 public class Workers extends Base {
-
+    @Context
+    private SecurityContext securityContext;
+    
     @RolesAllowed({ "worker" })
     @PUT
     @Path("/hello")
@@ -35,9 +41,12 @@ public class Workers extends Base {
     @Path("/metrics")
     @Consumes(MediaType.APPLICATION_JSON)
     public void addWorkerMetric(List<WorkerMetricDTO> metrics) {
+        BackMeUpUser activeWorker = ((BackmeupPrincipal)securityContext.getUserPrincipal()).getUser();
+        
         List<WorkerMetric> metricsList = new ArrayList<WorkerMetric>(metrics.size());
         for(WorkerMetricDTO m : metrics) {
             WorkerMetric model = getMapper().map(m, WorkerMetric.class);
+            model.setWorkerId(Long.toString(activeWorker.getUserId()));
             metricsList.add(model);
         }
         
