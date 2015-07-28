6fdd3d92d602f520d7c7f5347546eb954ad9b12b
Sebastian Pimminger <sebastian.pimminger@gmail.com>
2015-07-28 14:43:47 +0200

    Ignore worker management methods for now

--- backmeup-core/src/main/java/org/backmeup/logic/WorkerLogic.java
+++ backmeup-core/src/main/java/org/backmeup/logic/WorkerLogic.java
@@ -7,6 +7,12 @@ import org.backmeup.model.WorkerMetric;
 import org.backmeup.model.dto.WorkerConfigDTO;
 
 public interface WorkerLogic {
+    
+//    String register(String workerId, String workerSecret);
+//    
+//    String delete(String workerId);
+//    
+//    Token authorize(String workerId, String workerSecret);
 
     WorkerConfigDTO initializeWorker(WorkerInfo workerInfo);
 
